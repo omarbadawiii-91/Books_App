62e7ccca6deae42f80b7a99af0f75dda743d0177
omarbadawiii-91 <omarbadawe18@gmail.com>
2026-01-30 11:50:46 +0200

    Finished

--- lib/Features/screens/viewbook_screen/items/top_line_view_book_screen.dart
+++ lib/Features/screens/viewbook_screen/items/top_line_view_book_screen.dart
@@ -1,6 +1,6 @@
+import 'package:books_app/Features/screens/homepage_screen/homepage_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 
 class Top_line_view_book extends StatelessWidget {
   const Top_line_view_book({super.key});
@@ -14,7 +14,9 @@ class Top_line_view_book extends StatelessWidget {
             padding: EdgeInsets.only(top: 15.h, left: 20.w),
             child: IconButton(
               onPressed: () {
-                GoRouter.of(context).pushReplacement('/homepage');
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                  return HomepageScreen();
+                }));
               },
               icon: Icon(
                 Icons.clear_outlined,
